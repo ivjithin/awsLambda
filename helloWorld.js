18dd92414abcf1c5be7b4114a620e3c2e48329b5
Jithin I V <ivjithin@gmail.com>
2017-06-13 18:27:47 +0530

    Create helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -4,6 +4,6 @@ exports.handler = function(event, context) {
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
-    context.succeed(event.key3+"-"+event.key1+"-"+event.key2);  // Echo back the first key value
+    context.succeed(event.key3+"-"+event.key1+"-"+event.key2); 
     // context.fail('Something went wrong');
 }
